d09c21e82e20d1a697fd0fb0b6d18b86a61c8389
Schmidt Anton <schmidt.ajax@yandex.ru>
2023-08-30 16:47:02 +0700

    Asynchronous UDP server using handler chains

--- server.cpp
+++ server.cpp
@@ -1,32 +1,46 @@
 #include <boost/asio.hpp>
 #include <iostream>
-#include <exception>
 #include <iomanip>
 
 using namespace std;
 namespace asio = boost::asio;
+namespace sys = boost::system;
+
+class UDPAsyncServer {
+ public:
+  using socket = boost::asio::ip::udp::socket;
+  using endpoint = boost::asio::ip::udp::endpoint;
+  static const int BufferSize = 256;
+
+  UDPAsyncServer(asio::io_service &service, unsigned short port)
+      : socket_(service, endpoint{asio::ip::udp::v4(), port}) {
+    WaitForReceive();
+  }
+
+  void WaitForReceive() {
+    socket_.async_receive_from(asio::buffer(buffer_, BufferSize), remote_peer_,
+                               [this](const sys::error_code &ec, size_t sz) {
+                                 buffer_[sz] = 0;
+                                 cout << "Received "s << quoted(buffer_) << " from "s << remote_peer_ << endl;
+
+                                 WaitForReceive();
+
+                                 const char *msg = "Hello from server";
+                                 socket_.async_send_to(asio::buffer(msg, strlen(msg)), remote_peer_,
+                                                       [this](const sys::error_code &ec, size_t sz) {});
+                               });
+  }
+
+ private:
+  socket socket_;
+  endpoint remote_peer_;
+  char buffer_[BufferSize];
+};
 
 int main() {
-  const unsigned short port = 55000;
-  const string greet{"Hello, World!"};
-
   asio::io_service service;
-  asio::ip::udp::endpoint endpoint{asio::ip::udp::v4(), port};
-  asio::ip::udp::socket socket{service, endpoint};
-  asio::ip::udp::endpoint ep;
-
-  while (true) {
-    try {
-      char msg[256];
-      size_t recvd = socket.receive_from(asio::buffer(msg, sizeof(msg)), ep);
-      msg[recvd] = 0;
-      cout << "Received "s << quoted(msg) << " from "s << ep.address() << ':' << ep.port() << endl;
-
-      socket.send_to(asio::buffer(greet), ep);
-    } catch (const exception &e) {
-      cerr << e.what() << endl;
-    }
-  }
+  UDPAsyncServer server{service, 55000};
+  service.run();
 
   return 0;
 }
