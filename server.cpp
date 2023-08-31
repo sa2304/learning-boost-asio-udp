18fd8aa6b6c99ee0db64195c5c9d72587db1efd1
Schmidt Anton <schmidt.ajax@yandex.ru>
2023-08-31 22:08:12 +0700

    Make UDP server multithreaded

--- server.cpp
+++ server.cpp
@@ -1,9 +1,10 @@
 #include <boost/asio.hpp>
 #include <boost/asio/spawn.hpp>
 #include <boost/bind.hpp>
+#include <boost/shared_array.hpp>
+#include <boost/thread.hpp>
 #include <memory>
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 namespace asio = boost::asio;
@@ -14,28 +15,49 @@ static const size_t BufferSize = 256;
 
 using shared_udp_socket = shared_ptr<udp::socket>;
 
-void udp_send_to(asio::yield_context yield, shared_udp_socket socket, udp::endpoint peer) {
+mutex mtx;
+void log(string message) {
+  lock_guard guard{mtx};
+  cout << "["s << boost::this_thread::get_id() << "] "s << message;
+}
+
+void udp_send_to(asio::yield_context yield, shared_udp_socket socket, udp::endpoint peer, boost::shared_array<char> buf,
+                 size_t size) {
+  log("udp_send_to\n");
   const char *msg = "Hello from server";
   socket->async_send_to(asio::buffer(msg, strlen(msg)), peer, yield);
+  socket->async_send_to(asio::buffer(buf.get(), size), peer, yield);
 }
 
 void udp_server(asio::yield_context yield, asio::io_service &service, unsigned short port) {
   shared_udp_socket socket = make_shared<udp::socket>(service, udp::endpoint{udp::v4(), port});
-  char buffer[BufferSize];
   udp::endpoint remote_peer;
   sys::error_code ec;
   while (true) {
-    socket->async_receive_from(asio::buffer(buffer, BufferSize), remote_peer, yield[ec]);
+    boost::shared_array<char> buffer{new char[BufferSize]};
+    log("Waiting for incoming data...\n");
+    size_t size = socket->async_receive_from(asio::buffer(buffer.get(), BufferSize), remote_peer, yield[ec]);
     if (!ec) {
-      spawn(service, boost::bind(udp_send_to, ::_1, socket, remote_peer));
+      spawn(service, boost::bind(udp_send_to, ::_1, socket, remote_peer, buffer, size));
     }
   }
 }
 
-int main() {
+int main(int argc, char** argv) {
+  log("Starting UDP server...\n");
   asio::io_service service;
   spawn(service, boost::bind(udp_server, ::_1, boost::ref(service), 55000));
-  service.run();
+  auto func = [&service] {
+    service.run();
+  };
+  int num_threads = (1 < argc) ? atoi(argv[1]) : 1;
+  if (num_threads <= 0) { num_threads = 1; }
+  cout << "Running "s << num_threads << " threads...\n";
+  boost::thread_group pool;
+  for (int i = 0; i < num_threads; ++i) {
+    pool.create_thread(func);
+  }
+  pool.join_all();
 
   return 0;
 }
